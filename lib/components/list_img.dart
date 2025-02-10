1e403b84220d8c6296799c5c7464d7747bed59b8
Anshumaankhare2403 <anshumaan24khare1@gmail.com>
2025-02-10 16:31:13 +0530

    ui

--- lib/components/list_img.dart
+++ lib/components/list_img.dart
@@ -8,7 +8,7 @@ class ListImg extends StatefulWidget {
 }
 
 class _ListImgState extends State<ListImg> {
-  final List<String> Dark = [
+  List<String> images = [
     "assets/image/dark/Dark (1).jpg",
     "assets/image/dark/Dark (2).jpg",
     "assets/image/dark/Dark (3).jpg",
@@ -23,48 +23,58 @@ class _ListImgState extends State<ListImg> {
     "assets/image/dark/Dark (12).jpg",
     "assets/image/dark/Dark (13).jpg",
     "assets/image/dark/Dark (14).jpg",
+    "assets/image/light/Light (1).jpg",
+    "assets/image/light/Light (2).jpg",
+    "assets/image/light/Light (3).jpg",
+    "assets/image/light/Light (4).jpg",
+    "assets/image/light/Light (5).jpg",
+    "assets/image/light/Light (6).jpg",
+    "assets/image/light/Light (7).jpg",
+    "assets/image/light/Light (8).jpg",
+    "assets/image/light/Light (9).jpg",
+    "assets/image/light/Light (10).jpg",
+    "assets/image/light/Light (11).jpg",
+    "assets/image/light/Light (12).jpg",
+    "assets/image/light/Light (13).jpg",
+    "assets/image/light/Light (14).jpg",
   ];
-  final List<String> Light = [
-    "assets/image/Light/Light (1).jpg",
-    "assets/image/Light/Light (2).jpg",
-    "assets/image/Light/Light (3).jpg",
-    "assets/image/Light/Light (4).jpg",
-    "assets/image/Light/Light (5).jpg",
-    "assets/image/Light/Light (6).jpg",
-    "assets/image/Light/Light (7).jpg",
-    "assets/image/Light/Light (8).jpg",
-    "assets/image/Light/Light (9).jpg",
-    "assets/image/Light/Light (10).jpg",
-    "assets/image/Light/Light (11).jpg",
-    "assets/image/Light/Light (12).jpg",
-    "assets/image/Light/Light (13).jpg",
-    "assets/image/Light/Light (14).jpg",
-  ];
-
-  int le = 24;
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        childAspectRatio: 0.687, // Default ratio
-      ),
-      itemCount: le,
-      padding: const EdgeInsets.all(10),
-      itemBuilder: (context, index) {
-        var me = index == 10 ? {Dark[index]} : {Light[index]};
+    double screenWidth = MediaQuery.of(context).size.width;
 
-        return Container(
-          decoration: BoxDecoration(
-            image:
-                DecorationImage(image: AssetImage('${me}'), fit: BoxFit.cover),
-            borderRadius: BorderRadius.circular(10),
-          ),
-        );
-      },
+    // Dynamically set crossAxisCount based on screen width
+    int crossAxisCount = screenWidth < 600
+        ? 2
+        : screenWidth < 900
+            ? 3
+            : 4;
+
+    // Adjust childAspectRatio dynamically
+    double childAspectRatio = screenWidth < 600 ? 0.7 : 0.8;
+
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: crossAxisCount,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: childAspectRatio,
+        ),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(images[index]), // ✅ Responsive image loading
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          );
+        },
+      ),
     );
   }
 }
